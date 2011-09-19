e38a8c3a128b38371d9e0746a49be7153e05ccd3
incorvia <incorvia@gmail.com>
2011-09-19 19:11:10 -0400

    reduced redundancy & added help

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    @base_title = "Chirper App"
+  end
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -24,24 +28,38 @@ describe PagesController do
     end
   end 
   
-  describe "Title Test: Home" do
-    it "it should have the right title" do
-      get 'home'
-      response.should have_selector("title", :content => "Chirper App | Home")
+  describe "Get 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
     end
   end
   
+  describe "Title Test: Home" do
+    it "it should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => @base_title + " | Home")
+    end
+  end
+
   describe "Title Test: About" do
     it "it should have the right title" do
       get 'about'
-      response.should have_selector("title", :content => "Chirper App | About")
+      response.should have_selector("title", :content => @base_title + " | About")
     end
   end
   
   describe "Title Test: Contact" do
     it "it should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => "Chirper App | Contact")
+      response.should have_selector("title", :content => @base_title + " | Contact")
     end
   end
+  
+  describe "Title Test: Help" do
+    it "it should have the right title" do
+      get 'help'
+      response.should have_selector("title", :content => @base_title + " | Help")
+    end
+  end    
 end
