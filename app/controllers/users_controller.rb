5709665a5a702e10580bebdd5483dbe3ab9f1148
incorvia <incorvia@gmail.com>
2011-10-03 14:28:27 -0400

    completed chapter 11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-before_filter :authenticate, :only => [:index, :edit, :update]
+before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
 before_filter :correct_user, :only => [:edit, :update]
+before_filter :admin_user,   :only => :destroy
 
   
   def new
@@ -45,6 +46,13 @@ before_filter :correct_user, :only => [:edit, :update]
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+
+ 
 
 
 private
@@ -58,5 +66,8 @@ private
     redirect_to(root_path) unless current_user?(@user)
   end
 
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
 
 end
