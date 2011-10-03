9bdb3835fafa01c318765461c02ec867dec112a1
incorvia <incorvia@gmail.com>
2011-10-03 15:30:53 -0400

    updated faker require

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
+
+  has_many  :microposts
   
   email_regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
   
