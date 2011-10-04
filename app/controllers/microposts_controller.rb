d25de9eb1fb4dea9405cf195515ee6099efb55e2
incorvia <incorvia@gmail.com>
2011-10-04 17:21:18 -0400

    delete users

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
 
 before_filter :authenticate
+before_filter :authorized_user, :only => :destroy
 
   def create
     @micropost  = current_user.microposts.build(params[:micropost])
@@ -14,6 +15,14 @@ before_filter :authenticate
   end
 
   def destroy
+    @micropost.destroy
+    redirect_back_or root_path
   end
 
+private
+
+  def authorized_user
+    @micropost = Micropost.find(params[:id])
+    redirect_to root_path unless current_user?(@micropost.user)
+  end
 end
