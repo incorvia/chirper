4c17a9aeb19ca27313f4d537a7c1c14ae5c3be5f
incorvia <incorvia@gmail.com>
2011-10-04 15:58:44 -0400

    re-routed sign-in to home rather than profile

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -53,9 +53,9 @@
           controller.should be_signed_in
         end
 
-        it "should redirect to the user show page" do
+        it "should redirect to the user root" do
           post :create, :session => @attr
-          response.should redirect_to(user_path(@user))
+          response.should redirect_to(root_url )
         end
       end
     end
